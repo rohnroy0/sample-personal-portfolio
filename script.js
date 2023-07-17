566b5675cd6a95469fbae9095405975b032793bd
github_username <you@example.com>
2023-07-17 10:40:10 +0530

    final

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $("#submit-form").validate({
+        rules:{
+            fname:{
+                required:true,
+                minlength:2
+            },
+            place:{
+                required:true,
+                minlength:2
+            },
+            email:{
+                required:true,
+                email:true
+            },
+            mobile:{
+                required:true,
+                minlength:10,
+                maxlength:10
+
+            }
+            
+        },
+        messages:{
+            fname:{
+                minlength:"enter a valid name"
+            },
+            place:{
+                minlenth:"enter a valid place"
+            },
+            mobile:{
+                minlength:"enter a valid number",
+                maxlength:"enter a valid number"
+            }
+            
+        }
+    })
+})
